09053fac0427eecb4cf067d3edd71109e0617c9e
moanrisy <moanrisy@gmail.com>
2022-04-25 16:51:23 +0700

    Add authready

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,31 +4,36 @@ import { useContext } from "react";
 import AuthContext from "../stores/authContext";
 
 const Navbar = () => {
-  const { user, login, logout } = useContext(AuthContext);
+  const { user, login, logout, authReady } = useContext(AuthContext);
   return (
     <nav>
       <div className="logo">
         <Image alt="logo" src="/logo.png" width={65} height={77} />
       </div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <Link href="/ninjas">
-        <a>Ninja Listing</a>
-      </Link>
-      {!user && (
-        <a onClick={login} className="button">
-          Login/Signup
-        </a>
-      )}
-      {user && <a>{user.email}</a>}
-      {user && (
-        <button onClick={logout} className="button">
-          Logout
-        </button>
+      {authReady && (
+        <div>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          <Link href="/ninjas">
+            <a>Ninja Listing</a>
+          </Link>
+
+          {!user && (
+            <a onClick={login} className="button">
+              Login/Signup
+            </a>
+          )}
+          {user && <a>{user.email}</a>}
+          {user && (
+            <button onClick={logout} className="button">
+              Logout
+            </button>
+          )}
+        </div>
       )}
     </nav>
   );
